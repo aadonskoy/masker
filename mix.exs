88bcb835d296aa4dbabf2da5b4d5d7b42a8d2059
Oleksii Donskoi <aadonskoy@gmail.com>
2021-11-10 19:40:23 +0200

    Update docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule Masker.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Masker",
+      description: "Tool for finding a 2d pattern in a 2d list.",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/aadonskoy/masker"}
+      ]
     ]
   end
 
@@ -21,8 +27,7 @@ defmodule Masker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
